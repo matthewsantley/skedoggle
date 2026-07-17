45cb87d23f81ddcd78587564750b18f098e48da7
matthewsantley <mattsantley@hotmail.com>
2026-07-17 23:59:19 +0100

    Update BuddybossCustomCode.h

--- ios/BuddybossCustomCode.h
+++ ios/BuddybossCustomCode.h
@@ -1,7 +1,9 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTRootView.h>
+#import <React/RCTEventEmitter.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface BuddybossCustomCode : NSObject <RCTBridgeModule>
+@interface BuddybossCustomCode : RCTEventEmitter <RCTBridgeModule, CLLocationManagerDelegate>
 
 + (void)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions withBridge:(RCTBridge *)bridge;
 + (void)rootViewVisible:(RCTRootView *)rootView;
